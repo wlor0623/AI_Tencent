5354c917110cb7419138d44dc4b90e6699ee458f
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2019-12-16 11:02:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-const md5 = require('md5');
-//获取签名
-function getReqSign(params, appkey) {
-  let sortParams = jsonSort(params);
-  let str = "";
-  for (const key in sortParams) {
-    if (sortParams[key]) {
-      str = str + key + "=" + encodeURI(sortParams[key]) + "&"
-    }
-  }
-  str = str + "app_key=" + appkey;
-  let sign = md5(str).toUpperCase();
-  return sign
-}
+const http = require("./http");
 
-//key升序
-function jsonSort(jsonData) {
-  try {
-    let tempJsonObj = {};
-    let sdic = Object.keys(jsonData).sort();
-    sdic.map((item, index) => {
-      tempJsonObj[item] = jsonData[sdic[index]]
+function translate(text) {
+  http.post('/nlp/nlp_texttranslate ', {
+      text: text,
+      source: "zh",
+      target: "en"
     })
-    return tempJsonObj;
-  } catch (e) {
-    return jsonData;
-  }
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {});
 }
 
-let params = {
-  app_id: "10000",
-  key1: "腾讯AI开放平台",
-  key2: "示例仅供参考",
-  nonce_str: "20e3408a79",
-  time_stamp: "1493449657",
-}
-let appkey = 'a95eceb1ac8c24ee28b70f7dbba912bf';
-console.log(getReqSign(params, appkey));
+translate("你好")
